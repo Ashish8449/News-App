a5c90369a44693d5f9d88e9d43c1c4d8f50a5eb6
ashish8449 <ashishgururani8449@gmail.com>
2022-02-07 18:09:50 +0530

    addedjs

--- script.js
+++ script.js
@@ -91,9 +91,33 @@ const mainPage = function (data) {
       </div>
 
       <div class="container-bottom-left">
-       
-         ${addElements(data.splice(15, 20), cardWithBg)}
-     
+        <article>
+          <img src="images/bottom-left-1.jpg" />
+          <div>
+            <h3>Best smart speakers for the year</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Commodi iure modi animi cupiditate. Explicabo, nihil?
+            </p>
+
+            <a href="#">Read More <span>>></span></a>
+          </div>
+        </article>
+
+        <article>
+          <img src="images/bottom-left-2.jpg" />
+          <div>
+            <h3>
+              iPad Pro, reviewed: Has the iPad become my main computer now?
+            </h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Commodi iure modi animi cupiditate. Explicabo, nihil?
+            </p>
+
+            <a href="#">Read More <span>>></span></a>
+          </div>
+        </article>
       </div>
     </section>
 
